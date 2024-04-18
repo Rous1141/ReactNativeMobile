38356ad7b192c3c0f4214b80a11dfb4bb4d01759
Rous1141 <132892586+Rous1141@users.noreply.github.com>
2024-04-19 01:23:48 +0700

    Add a button

--- ReactNative/Session01/myfirstapp/components/WelcomeScreen.jsx
+++ ReactNative/Session01/myfirstapp/components/WelcomeScreen.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-import { View, Text, Image, StyleSheet, TextInput } from 'react-native'
+import { View, Text, Image, StyleSheet, TextInput,Button, Alert, } from 'react-native'
+import Separator from './Customized/Separator'
 export default function WelcomeScreen() {
     const [text, setText] = useState('')
     return (
@@ -15,18 +16,28 @@ export default function WelcomeScreen() {
                 <Text style={customStyles.textBody}>
                     Greeting, human of Earth. This is the almighty Lunarian Corps of the Moon. I'm Sagume Kishin, heron of lunatic, ambassidor of Watasuki's Clan. And I want you, to be a part of the Lunarian Army! We got cute lunar rabbits as the frontline units!
                 </Text>
+                <Separator/>
+                <View 
+                    style={customStyles.form}
+                >
                 <TextInput
                     value={text}
                     onChangeText={(text) => setText(text)}
                     style={customStyles.textInput}
                     placeholder='Enter your name to participate...'
                 />
+                <Button
+                    color="white"
+                    title='Submit'
+                    onPress={()=> {Alert.alert(`You are: ${text}`);name = text}}
+                />
+                
+                </View>
                 {text &&
                     (<Text>
-                        {text}
+                        Welcome, {text}!
                     </Text>)
                 }
-
             </View>
         </>
     )
@@ -66,10 +77,16 @@ const customStyles = StyleSheet.create({
     },
     textInput: {
         backgroundColor: "rgba(255, 255, 255, 0.8)",
-        border: "1px solid black",
-        borderRadius: "2px",
-        marginTop: "10%",
-        width: "80%",
-        paddingLeft:"1%"
+        borderColor: "black",
+        borderRadius: 2,
+        marginTop: 5,
+        width: "50%",
+        paddingLeft:"1%",
+        height:30,
     },
+    form:{
+        display:'flex',
+        flexDirection:'row',
+        justifyContent:'space-evenly'
+    }
 })
