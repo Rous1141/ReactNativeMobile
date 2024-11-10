fa5d7b062c77d5f062111be77f5abdbaba3eb2c3
Rous1141 <132892586+Rous1141@users.noreply.github.com>
2024-11-10 21:52:51 +0700

    First Page

--- ReactNative/spell-card/app/(tabs)/index.tsx
+++ ReactNative/spell-card/app/(tabs)/index.tsx
@@ -1,38 +1,70 @@
 import { Text, View } from "react-native";
 import { StyleSheet } from "react-native";
 import { Link } from "expo-router";
+import { Image } from "expo-image";
+import ImageViewer from "@/components/ImageViewer";
+import Button from "@/components/Button";
+import * as ImagePicker from 'expo-image-picker'
+import { useState } from "react";
+const placeHolderImages = require("@/assets/images/background-image.png")
 export default function Index() {
-  const styles = StyleSheet.create({
-    container:{
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-      backgroundColor: "#25292e"
-    },
-    text: {
-      fontSize: 30,
-      color: "white",
-    },
-    button:{
-      marginTop: 20,
-      padding: 10,
-      backgroundColor: "blue",
-      borderRadius: 10,
-      color: "white",
-      fontWeight: "bold"
+  const [selectedImage,setSelectedImage] = useState<string|undefined>(undefined);
+
+
+  const chooseImage = async () =>{
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes:ImagePicker.MediaTypeOptions.Images,
+      allowsEditing:true, // User cna crop image before sent
+      quality:1
+    })
+
+    if(!result.canceled){
+      setSelectedImage(result.assets[0].uri) // result is an array of selectio  or undefined
+    }else{
+      alert("You did not choose any image")
     }
-  })
+  }
+  
   return (
     <View
     style= {styles.container}
     >
-      <Text
-        style= {styles.text}
-      >
-        Hello World, This is the homescreen.
-      </Text>
-      <Link style={styles.button} href={"/aboutus"}>Go To About Page</Link>
+      <ImageViewer
+        imgSource={placeHolderImages}
+        selectedImage={selectedImage}
+      />
+      <Button
+      onPress={chooseImage}
+        theme="primary"
+        label={"Select A Photo"}
+      />
+       <Button
+        label={"Use This Photo"}
+      />
+    
 
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#25292e"
+  },
+  text: {
+    fontSize: 30,
+    color: "white",
+    textAlign:"center"
+  },
+  button:{
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: "green",
+    borderRadius: 10,
+    color: "white",
+    fontWeight: "bold",
+  },
+})
